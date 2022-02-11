a0ec73efa7f6f99d938b9f606458f4a122c1f634
Jihye-Son <zzlhye_@naver.com>
2022-02-11 09:40:13 +0900

    deepsee project

---
+++ src/main/java/com/semiproject1/mapper/UserMapper.java
@@ -0,0 +1,32 @@
+package com.semiproject1.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import com.semiproject1.user.vo.UserVo;
+
+@Mapper
+public interface UserMapper {
+
+	@Select("select email from member")
+	public List<UserVo> getUserList();
+	
+	@Insert("insert into member values(#{in.email}, #{in.pwd}, #{in.name}, #{in.phoneNumber}, #{in.birthDate})")
+	public int createUser(@Param("in") UserVo userVo);
+	
+	@Select("select * from member where email = #{userVo.email} and pwd = #{userVo.pwd}")
+	public UserVo login(@Param("userVo") UserVo userVo);
+	
+	@Select("select email from member where email = #{check.email}")
+	public UserVo emailCheck(@Param("check") UserVo userVo);
+	
+}
+
+
+
+
+
